709af2231d2d227e84462dc49f0c1493591c39f3
JuanMiguel <capitanisaza@gmail.com>
2021-11-15 17:54:35 -0500

    Integracion con App equipo ciclo 4

---
+++ app/src/main/java/com/example/my_first_mobile_app/Sign_Up.kt
@@ -0,0 +1,11 @@
+package com.example.my_first_mobile_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Sign_Up : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
